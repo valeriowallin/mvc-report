84f7d2c9992fb638c7c42eef178bd3807cd257a7
Valério Wallin <valerio.wallin@proton.me>
2025-04-22 08:21:15 +0200

    symfony installation

---
+++ symfony/app/src/Controller/LuckyController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController
+{
+    #[Route('/lucky/number')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+    #[Route('/lucky')]
+    public function lucky(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
